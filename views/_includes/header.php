71d994aced4389dd178792e7f0bbf8601bc1729c
vagner1984 <vagnerl@dhermon.com.br>
2018-10-22 15:27:50 -0300

    Ajustando a lista de Users

--- views/_includes/header.php
+++ views/_includes/header.php
@@ -18,6 +18,18 @@
 
 	<link rel="stylesheet" href="<?php echo HOME_URI;?>/views/_css/style.css">
 	<link rel="stylesheet" href="<?php echo HOME_URI;?>/bootstrap-3.3.7-dist/css/bootstrap.css">
+	<!-- Bootstrap Core CSS -->
+	<link href="<?php echo HOME_URI;?>/dashboard/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- MetisMenu CSS -->
+	<link href="<?php echo HOME_URI;?>/dashboard/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+	<!-- DataTables CSS -->
+	<link href="<?php echo HOME_URI;?>/dashboard/vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+	<!-- DataTables Responsive CSS -->
+	<link href="<?php echo HOME_URI;?>/dashboard/vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+	<!-- Custom CSS -->
+	<link href="<?php echo HOME_URI;?>/dashboard/dist/css/sb-admin-2.css" rel="stylesheet">
+	<!-- Custom Fonts -->
+	<link href="<?php echo HOME_URI;?>/dashboard/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
 	<!--[if lt IE 9]>
 	<script src="<?php echo HOME_URI;?>/views/_js/scripts.js"></script>
--- views/user-register/user-register-view.php
+++ views/user-register/user-register-view.php
@@ -3,6 +3,7 @@
 <div class="wrap">
 
 <?php
+
 // Carrega todos os métodos do modelo
 $modelo->validate_register_form();
 $modelo->get_register_form( chk_array( $parametros, 1 ) );
@@ -13,7 +14,7 @@ $modelo->del_user( $parametros );
 	<table class="form-table">
 		<tr>
 			<td>Name: </td>
-			<td> <input type="text" name="user_name" value="<?php 
+			<td> <input type="text" name="user_name" value="<?php
 				echo htmlentities( chk_array( $modelo->form_data, 'user_name') );
 				?>" /></td>
 		</tr>
@@ -45,9 +46,9 @@ $modelo->del_user( $parametros );
 	</table>
 </form>
 
-<?php 
+<?php
 // Lista os usuários
-$lista = $modelo->get_user_list(); 
+$lista = $modelo->get_user_list();
 ?>
 
 
@@ -61,28 +62,64 @@ $lista = $modelo->get_user_list();
 			<th>Edição</th>
 		</tr>
 	</thead>
-			
+
 	<tbody>
-			
+
 		<?php foreach ($lista as $fetch_userdata): ?>
 
 			<tr>
-			
+
 				<td> <?php echo $fetch_userdata['user_id'] ?> </td>
 				<td> <?php echo $fetch_userdata['user'] ?> </td>
 				<td> <?php echo $fetch_userdata['user_name'] ?> </td>
 				<td> <?php echo implode( ',', unserialize( $fetch_userdata['user_permissions'] ) ) ?> </td>
-				
-				<td> 
+
+				<td>
 					<a href="<?php echo HOME_URI ?>/user-register/index/edit/<?php echo $fetch_userdata['user_id'] ?>">Edit</a>
 					<a href="<?php echo HOME_URI ?>/user-register/index/del/<?php echo $fetch_userdata['user_id'] ?>">Delete</a>
 				</td>
 
 			</tr>
-			
+
 		<?php endforeach;?>
-			
+
 	</tbody>
 </table>
+
+<div class="panel panel-default">
+		<div class="panel-heading">
+				Lista de Usuarios
+		</div>
+		<!-- /.panel-heading -->
+		<div class="panel-body">
+				<table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+						<thead>
+								<tr>
+										<th>ID</th>
+										<th>Usuario</th>
+										<th>Nome</th>
+										<th>Permissoes</th>
+										<th><center>Editar</center></th>
+										<th><center>Excluir</center></th>
+								</tr>
+						</thead>
+						<tbody>
+							<?php foreach ($lista as $fetch_userdata): ?>
+								<tr class="odd gradeX">
+										<td><?php echo $fetch_userdata['user_id'] ?></td>
+										<td><?php echo $fetch_userdata['user'] ?></td>
+										<td><?php echo $fetch_userdata['user_name'] ?></td>
+										<td><?php echo implode( ',', unserialize( $fetch_userdata['user_permissions'] ) ) ?></td>
+										<td align="center"><a href="<?php echo HOME_URI ?>/user-register/index/edit/<?php echo $fetch_userdata['user_id'] ?>"><i class="fa fa-edit"></i></a></td>
+										<td align="center"><a href="<?php echo HOME_URI ?>/user-register/index/del/<?php echo $fetch_userdata['user_id'] ?>"><i class="fa fa-trash"></i></a></td>
+								</tr>
+							<?php endforeach;?>
+
+
+						</tbody>
+				</table>
+		</div>
+		<!-- /.panel-body -->
+</div>
 
 </div> <!-- .wrap -->
